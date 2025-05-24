6bbc7e8711f4ff0673e5eb5790e0df03d8ce890b
Jefferson Vega <jefferson.vegag@gmail.com>
2025-05-24 15:37:13 -0500

    Implementar tienda de plantas con React y Redux to netlify 2.1

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: undefined,
-      },
-    },
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.[jt]sx?$/,
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
   }
 })
